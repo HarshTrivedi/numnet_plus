8c460adba87be9eda06747cd07df1c7625217c51
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-03-29 22:24:48 -0400

    typo

--- train_beaker.sh
+++ train_beaker.sh
@@ -56,7 +56,7 @@ else
 fi
 
 DATA_CONFIG="--data_dir ${CACHE_DIR} --save_dir ${SAVE_DIR}"
-TRAIN_CONFIG="--batch_size ${BATCH} --eval_batch_size ${BATCH} --max_epoch ${EPOCHS} --num_instances_per_epoch {NUM_INSTANCES} \
+TRAIN_CONFIG="--batch_size ${BATCH} --eval_batch_size ${BATCH} --max_epoch ${EPOCHS} --num_instances_per_epoch ${NUM_INSTANCES} \
               --warmup 0.06 --optimizer adam  --learning_rate ${LR} --weight_decay ${WD} --seed ${SEED} \
               --gradient_accumulation_steps ${GRAD} --bert_learning_rate ${BLR} --bert_weight_decay ${BWD} \
               --log_per_updates 100 --eps 1e-6 ${PRE_PATH}"
